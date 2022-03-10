ee2abe7bc06cb00295f5ac04016aa3e85225e88c
Frost <staikartur@mail.ru>
2022-03-10 22:01:06 +0500

    7

---
+++ Task7/exercise_3.cpp
@@ -0,0 +1,31 @@
+#include "exercise_3.hpp"
+#include <numeric>
+
+void exercise_3()
+{
+    exercise_2::FullName fn;
+    fn.set_name("Ivan");
+    fn.set_surname("Ivanov");
+    fn.set_patronymic("Ivanovich");
+
+    exercise_2::Student s;
+    *s.mutable_name() = fn;
+    s.add_grades(5);
+    s.add_grades(2);
+    s.add_grades(4);
+    s.add_grades(3);
+    s.add_grades(4);
+    s.add_grades(3);
+    s.add_grades(4);
+    s.add_grades(3);
+    s.set_avg_score(std::accumulate(s.grades().begin(), s.grades().end(), 0) / s.grades().size());
+
+    SG::StudentsGroup sg;
+    sg.add_student(s);
+    sg.Save();
+
+    SG::StudentsGroup new_sg;
+    new_sg.Open();
+    std:: cout << new_sg.GetAllInfo(fn) << std::endl;
+}
+
