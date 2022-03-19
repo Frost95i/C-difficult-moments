7b1f1940386b3f25356e37e1bcd911320cf08e50
Frost <staikartur@mail.ru>
2022-03-19 11:53:25 +0500

    main

--- Task8/Task8.cpp
+++
@@ -1,9 +0,0 @@
-﻿#include "lesson_1.hpp"
-#include "tests.hpp"
-#include "gtest/gtest.h"
-
-int main(int argc, char** argv)
-{
-    testing::InitGoogleTest(&argc, argv);
-    return RUN_ALL_TESTS();
-}
